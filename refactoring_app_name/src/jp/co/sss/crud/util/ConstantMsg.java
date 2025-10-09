b2e8058bec5ae71f4ac45d2894ced51b349b5774
y-kinugawa <y.kinugawa.its.202509@gmail.com>
2025-10-09 11:36:02 +0900

    3.add:ConstantMsg.java メッセージの定数化。
    狙い:信頼性、拡張性の向上

---
+++ refactoring_app_name/src/jp/co/sss/crud/util/ConstantMsg.java
@@ -0,0 +1,53 @@
+package jp.co.sss.crud.util;
+
+/**
+ * メッセージをまとめたクラス(コンソール出力)
+ * 
+ * @author 絹川
+ */
+
+public class ConstantMsg {
+
+	/** インスタンス化を禁止 */
+	private ConstantMsg() {
+	}
+
+	/**
+	 * 案内メッセージ
+	 */
+	/**タイトル*/
+	public static final String MENU_TITLE = "=== 社員管理システム ===";
+	/**メニュー入力*/
+	public static final String MENU_GUIDE = "メニュー番号を入力してください：";
+	/**更新入力*/
+	public static final String UPDATE_GUIDE = "更新する社員の社員IDを入力してください：";
+	/**削除入力*/
+	public static final String DELETE_GUIDE = "削除する社員の社員IDを入力してください：";
+	/**システム終了*/
+	public static final String END_GUIDE = "システムを終了します。";
+
+	/**
+	 * エラーメッセージ
+	 */
+	/**非該当*/
+	public static final String NO_EMPLOYEE = "該当者はいませんでした";
+	/**半角数字入力*/
+	public static final String NUMBER_VALIDATION = "半角数字で入力してください";
+	/**20文字制限*/
+	public static final String SIZE_VALIDATION = "20文字以内で入力してください";
+	/**範囲指定*/
+	public static final String RANGE_VALIDATION = " のいずれかを入力してください";
+	/**日付形式*/
+	public static final String DATE_VALIDATION = "(yyyy/mm/dd)の形式で入力してください";
+
+	/**
+	 * 完了メッセージ
+	 */
+	/**登録*/
+	public static final String COMP_INSERT = "社員情報を登録しました";
+	/**更新*/
+	public static final String COMP_UPDATE = "社員情報を更新しました";
+	/**削除*/
+	public static final String COMP_DELETE = "社員情報を削除しました";
+
+}
